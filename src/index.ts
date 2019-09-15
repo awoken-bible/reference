d74f2e8e1ec08ca288cabf20b5ae469bef7a25b0
Jamie Terry <jnterry@ntlworld.com>
2019-09-15 19:09:08 +0100

    Fix hybrid interface exported by index.js

--- src/index.ts
+++ src/index.ts
@@ -14,21 +14,14 @@ import { FormatOptions } from './printer';
 export { Versification } from './Versification';
 export { FormatOptions } from './printer';
 
+/**
+ * Publically exposed interface to this library
+ */
 export interface BibleRefLib {
 	/**
 	 * Versification used by all methods
 	 */
 	versification : Versification;
-
-	/**
-	 * Constructor interface
-	 *
-	 * We are using a hybrid interface to this lib, you can just require(lib)
-	 * and call function such as parse, etc - or you can construct a new instance
-	 * with a non standard versification scheme
-	 */
-	(this: BibleRefLib) : BibleRefLib;
-
 
 	/**
 	 * Parses a string containing a BibleRef
@@ -48,30 +41,48 @@ export interface BibleRefLib {
 	format(b : BibleRef | BibleRef[], opts?: FormatOptions) : string;
 }
 
-const BibleRefLib = function(this: BibleRefLib){
+function parse(this: BibleRefLib, str: string) : ParseResult{
+	return Parsers.BibleRef.parse(str);
+}
+
+function parseOrThrow(this: BibleRefLib, str: string) : BibleRef[]{
+	let result = this.parse(str);
+	if(result.status === true){ return result.value; }
+	throw result;
+}
+
+function format(this: BibleRefLib, b : BibleRef | BibleRef[], opts?: FormatOptions) : string{
+	if('length' in b){
+		return Printer.formatBibleRefList(b, opts);
+	}
+
+	if(b.is_range){
+		return Printer.formatBibleRange(b, opts);
+	} else {
+		return Printer.formatBibleVerse(b, opts);
+	}
+}
+
+/**
+ * Constructor interface
+ *
+ * We are using a hybrid interface to this lib, you can just require(lib)
+ * and call function such as parse, parseOrThrow, etc - or you can construct a
+ * new instance with a non standard versification scheme and then call methods
+ * of that
+ */
+type BibleRefLibConstructor =	(this: BibleRefLib) => BibleRefLib;
+
+const constructFunc : BibleRefLib & BibleRefLibConstructor = function(this: BibleRefLib) : BibleRefLib {
 	this.versification = VERSIFICATION;
+	this.parse         = parse;
+	this.parseOrThrow  = parseOrThrow;
+	this.format        = format;
+	return this;
+};
+constructFunc.versification = VERSIFICATION;
+constructFunc.parse         = parse.bind(constructFunc);
+constructFunc.parseOrThrow  = parseOrThrow.bind(constructFunc);
+constructFunc.format        = format.bind(constructFunc);
 
-	this.parse = (str: string) => {
-		return Parsers.BibleRef.parse(str);
-	};
-
-	this.parseOrThrow = (str: string) => {
-		let result = this.parse(str);
-		if(result.status === true){ return result.value; }
-		throw result;
-	};
-
-	this.format = (b : BibleRef | BibleRef[], opts?: FormatOptions) => {
-		if('length' in b){
-			return Printer.formatBibleRefList(b, opts);
-		}
-
-		if(b.is_range){
-			return Printer.formatBibleRange(b, opts);
-		} else {
-			return Printer.formatBibleVerse(b, opts);
-		}
-	};
-} as BibleRefLib;
-
-export default BibleRefLib;
+export default constructFunc;
--- test/index.js
+++ test/index.js
@@ -5,16 +5,20 @@ const expect   = chai.expect;
 
 const BibleRef = require('../src/index.ts').default;
 
-const bref = new BibleRef();
-
 describe("index", () => {
-  it('Parser / Printer Round Trips', () => {
+  it('Can use static methods or new() interface', () => {
+    const bref = new BibleRef();
 
     let str  = 'Genesis 1:10';
     let data = { book: 'GEN', chapter: 1, verse: 10 };
     expect(bref.parseOrThrow(str )).is.deep.equal([data]);
+    expect(bref.parse       (str )).is.deep.equal({ status: true, value: [data] });
     expect(bref.format      (data)).is.deep.equal(str);
     expect(bref.format      ([data])).is.deep.equal(str);
 
+    expect(BibleRef.parseOrThrow(str )).is.deep.equal([data]);
+    expect(BibleRef.parse       (str )).is.deep.equal({ status: true, value: [data] });
+    expect(BibleRef.format      (data)).is.deep.equal(str);
+    expect(BibleRef.format      ([data])).is.deep.equal(str);
   });
 });
