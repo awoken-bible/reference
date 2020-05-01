8ceb7a838df2d584a2f9f8db22312380398283e4
Jamie Terry <jt@terry.cloud>
2020-05-01 17:09:02 +0100

    Implement geometry functions

--- src/geometry.ts
+++ src/geometry.ts
@@ -1,49 +1,88 @@
 /**
  * Various utility functions for treating [[BibleRef]]s as geometry, IE:
- * BibleVerse's as points, and BibleRange's as line segments
+ * BibleVerse's as points, and BibleRange's as line segments, and then finding intersections/etc
  */
 
-import { BibleRef, BibleVerse, BibleRange }    from './BibleRef';
+import { BibleRef, BibleVerse, BibleRange, BibleRefLibData } from './BibleRef';
 import { Versification, BookMeta } from './Versification'
 import * as Vidx from './vidx';
+import { combineRanges } from './range-manip';
 
 export interface GeometryFunctions {
-	getIntersection(a: BibleRef | BibleRef[], b: BibleRef | BibleRef[]): BibleRef | null;
+	getIntersection(a: BibleRef | BibleRef[], b: BibleRef | BibleRef[]): BibleRef[];
 	intersects(a: BibleRef | BibleRef[], b: BibleRef | BibleRef[]): boolean,
 	contains(a: BibleRef, b: BibleRef): boolean,
-	getUnion(a: BibleRef | BibleRef[], b: BibleRef | BibleRef[]): BibleRef | null;
+	getUnion(a: BibleRef | BibleRef[], b: BibleRef | BibleRef[]): BibleRef[];
 };
 
-
-interface _Self {
-	versification : Versification;
-};
+/**
+ *  Represents a 1d line segment (we map [[BibleRange]]'s to vidx ranges to do maths on them)
+ *
+ * @private
+ */
+interface LineSegment {
+	min: number;
+	max: number;
+}
 
 /**
  * Creates a new array of [[BibleRef]]s which represents the intersection between two other sets
  *
  * @param this - Any object with a `versification` field
- * @param a    - First set of [[BibleRef]] instances
- * @param b    - Second set of [[BibleRef]] instances
+ * @param x    - First set of [[BibleRef]] instances
+ * @param y    - Second set of [[BibleRef]] instances
  *
  * @return Simplified and sorted list of [[BibleRef]]s which appear in both input sets. Will
  * return empty array if there are no verses in common between the inputs
  */
-export function getIntersection(this: _Self, a: BibleRef | BibleRef[], b: BibleRef | BibleRef[]) : BibleRef[]  {
-	return [];
+export function getIntersection(this: BibleRefLibData, x: BibleRef | BibleRef[], y: BibleRef | BibleRef[]) : BibleRef[]  {
+	let a = _toLineSegments(this, x);
+	let b = _toLineSegments(this, y);
+
+	let ai = 0; let bi = 0;
+	let out : BibleRef[] = [];
+	while(ai < a.length && bi < b.length){
+		// Find intersections between current list heads
+		let intersection = _intersectLineSegment(a[ai], b[bi]);
+
+		if(intersection === null){
+			// If no intersection between current heads, skip past the one which ends soonest
+			if(a[ai].max < b[bi].max){
+				++ai;
+			} else {
+				++bi;
+			}
+			continue;
+		}
+
+		// If intersection found, add to result set
+		out.push(_fromLineSegment(this, intersection));
+
+		// Alter the ranges to exclude that which we've just found
+		a[ai].min = intersection.max+1;
+		b[bi].min = intersection.max+1;
+
+		// Skip past list heads if they now have length 0
+		if(a[ai].min >= a[ai].max){ ++ai; }
+		if(b[bi].min >= b[bi].max){ ++bi; }
+	}
+
+	return out;
 }
 
 /**
  * Determines whether two sets of [[BibleRef]]s have any verses in common
  */
-export function intersects(this: _Self, a: BibleRef | BibleRef[], b: BibleRef | BibleRef[]) : boolean {
+export function intersects(this: BibleRefLibData, a: BibleRef | BibleRef[], b: BibleRef | BibleRef[]) : boolean {
 	return getIntersection.bind(this)(a,b).length > 0;
 }
 
 /**
  * Determines whether `outer` fully contains all verses present within `inner`
  */
-export function contains(this: _Self, a: BibleRef | BibleRef[], b: BibleRef | BibleRef[]) : boolean {
+export function contains(this: BibleRefLibData, a: BibleRef | BibleRef[], b: BibleRef | BibleRef[]) : boolean {
+	let x = _toLineSegments(this, a);
+	let y = _toLineSegments(this, b);
 	return true;
 }
 
@@ -51,6 +90,52 @@ export function contains(this: _Self, a: BibleRef | BibleRef[], b: BibleRef | Bi
  * Returns the union of two sets of [[BibleRef]]s, IE: the combined and simpified set of verses
  * which are in one or the other or both input sets
  */
-export function getUnion(this: _Self, a: BibleRef | BibleRef[], b: BibleRef | BibleRef[]) : BibleRef[] {
-	return [];
+export function getUnion(this: BibleRefLibData, a: BibleRef | BibleRef[], b: BibleRef | BibleRef[]) : BibleRef[] {
+	let x = 'length' in a ? a : [a];
+	let y = 'length' in b ? b : [b];
+	return combineRanges.bind(this)([...x, ...y]);
+}
+
+/**
+ * Converts a set of [[BibleRef]]'s into a set of [[LineSegment]] instances
+ *
+ * @private
+ */
+function _toLineSegments(lib: BibleRefLibData, input: BibleRef | BibleRef[]): LineSegment[] {
+	let in_arr : BibleRef[] = 'length' in input ? input : [input];
+	return combineRanges.bind(lib)(in_arr).map((x : BibleRef) => {
+		if(x.is_range){
+			return { min: Vidx.toVidx(lib.versification, x.start), max: Vidx.toVidx(lib.versification, x.end) };
+		} else {
+			let val = Vidx.toVidx(lib.versification, x);
+			return { min: val, max: val };
+		}
+	});
+}
+
+function _fromLineSegment(lib: BibleRefLibData, line: LineSegment): BibleRef{
+	if(line.min === line.max){
+		return Vidx.fromVidx(lib.versification, line.min);
+	} else {
+		return { is_range : true,
+						 start    : Vidx.fromVidx(lib.versification, line.min),
+						 end      : Vidx.fromVidx(lib.versification, line.max)
+					 };
+	}
+}
+
+/**
+ * Finds the intersection between two 1d line segments, or returns null if there is no intersection
+ *
+ * @private
+ */
+function _intersectLineSegment(a: LineSegment, b: LineSegment) : LineSegment | null {
+	let min = Math.max(a.min, b.min);
+	let max = Math.min(b.max, a.max);
+
+	if(min <= max){
+		return { min, max };
+	} else {
+		return null;
+	}
 }
--- test/geometry.js
+++ test/geometry.js
@@ -1,28 +1,33 @@
 "use strict";
 
-const chai     = require('chai');
-const expect   = chai.expect;
+const chai      = require('chai');
+const expect    = chai.expect;
 
-const v        = require('../src/Versification.ts').default;
-const Parsers  = require('../src/parser.ts').default;
-const { getIntersection, intersects, getUnion } = require('../src/geometry.ts');
+const AwokenRef = require('../src/index.ts').default;
+
+const v         = require('../src/Versification.ts').default;
+const Parsers   = require('../src/parser.ts').default;
 
 function p(str){
-  return Parsers.BibleRef.parse(str).value;
+  let out = Parsers.BibleRef.parse(str);
+  if(out.value === undefined){
+    console.log("-----");
+    console.log(str);
+    console.dir(out);
+  }
+  return out.value;
 }
 
 describe("Geometry", () => {
   it('getIntersection', () => {
-
-    expect(getIntersection(v, p('Genesis 1:5-10'), p('Geneis 1:7-12'))).to.deep.equal([
+    expect(AwokenRef.getIntersection(p('Genesis 1:5-10'), p('Genesis 1:7-12'))).to.deep.equal([
       { is_range: true,
         start : { book: 'GEN', chapter: 1, verse:  7 },
         end   : { book: 'GEN', chapter: 1, verse: 10 },
       }
     ]);
-
-    expect(getIntersection(
-      v, p('Genesis 1:5-10'), p('Geneis 1:5, 8-9')
+    expect(AwokenRef.getIntersection(
+      p('Genesis 1:5-10'), p('Genesis 1:5,8-9')
     )).to.deep.equal([
       { book: 'GEN', chapter: 1, verse:  5 },
       { is_range: true,
@@ -31,24 +36,52 @@ describe("Geometry", () => {
       }
     ]);
 
-    expect(getIntersection(
-      v,
-      p('Gen 1:1,          Gen 1:3, Gen 1:6, Gen 1:7, Gen 1:10'),
-      p('         Gen 1:2, Gen 1:3,          Gen 1:7, Gen 1:10'),
+    expect(AwokenRef.getIntersection(
+      p('Gen 1:1;          Gen 1:3; Gen 1:6; Gen 1:7; Gen 1:10'.trim()),
+      p('         Gen 1:2; Gen 1:3;          Gen 1:7; Gen 1:10'.trim()),
     )).to.deep.equal([
       { book: 'GEN', chapter: 1, verse:  3 },
       { book: 'GEN', chapter: 1, verse:  7 },
       { book: 'GEN', chapter: 1, verse: 10 },
     ]);
+
+    expect(AwokenRef.getIntersection(
+      p('Joshua 24:14 - Judges 2:10; Judges 2:20 - 3:10'),
+      p('Joshua 22:1 - Joshua 24:20; Judges 2:5 - 2:23; Joshua 24:31'),
+    )).to.deep.equal([
+      { is_range : true,
+        start    : { book: 'JOS', chapter: 24, verse: 14 },
+        end      : { book: 'JOS', chapter: 24, verse: 20 },
+      },
+      { book: 'JOS', chapter: 24, verse: 31 },
+      { is_range: true,
+        start: { book: 'JDG', chapter: 2, verse:  5 },
+        end  : { book: 'JDG', chapter: 2, verse: 10 },
+      },
+      { is_range: true,
+        start: { book: 'JDG', chapter: 2, verse: 20 },
+        end  : { book: 'JDG', chapter: 2, verse: 23 },
+      },
+    ]);
   });
 
   it('intersects', () => {
-    expect(intersects(v, p('Gen 1'), p('Genesis 1:1'))).to.deep.equal(true);
-    expect(intersects(v, p('Exo 1'), p('Genesis 1:1'))).to.deep.equal(false);
+    expect(AwokenRef.intersects(p('Gen 1'), p('Genesis 1:1'))).to.deep.equal(true);
+    expect(AwokenRef.intersects(p('Exo 1'), p('Genesis 1:1'))).to.deep.equal(false);
   });
 
   it('getUnion', () => {
-    expect(getUnion(v, p('Gen 1:5-10'), p('Genesis 1:7-12'))).to.deep.equal(p('Gen 1:5-12'));
-    expect(getUnion(v, p('Gen 1:5   '), p('Genesis 1:7   '))).to.deep.equal(p('Gen 1:5, Gen 1:7'));
+    expect(AwokenRef.getUnion(p('Gen 1:5-10'), p('Genesis 1:7-12'))).to.deep.equal(p('Gen 1:5-12'));
+    expect(AwokenRef.getUnion(p('Gen 1:5   '), p('Genesis 1:7   '))).to.deep.equal(p('Gen 1:5; Gen 1:7'));
+
+    expect(AwokenRef.getUnion(
+      p('Joshua 24:14 - Judges 2:10; Judges 2:20 - 3:10'),
+      p('Joshua 22:1 - Joshua 24:20; Judges 2:5 - 2:23; Joshua 24:31'),
+    )).to.deep.equal([
+      { is_range : true,
+        start    : { book: 'JOS', chapter: 22, verse:  1 },
+        end      : { book: 'JDG', chapter:  3, verse: 10 },
+      }
+    ]);
   });
 });
