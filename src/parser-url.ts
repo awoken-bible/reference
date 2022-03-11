523d7aaab519a4722e3ac4f4dccdb689a23571dc
Jamie Terry <dev@terry.cloud>
2022-03-11 00:10:26 +0000

    Add support for cross-book ranges in url dedicated parser

--- src/parser-url.ts
+++ src/parser-url.ts
@@ -12,8 +12,9 @@ export function parseUrlEncoded(this: BibleRefLibData, raw: string): BibleRef[] 
 	//console.log('hello world');
 	let results : BibleRef[] = [];
 
-	// what is the active book in this block?
+	// variables to store context of the book/chapter for ranges/comma lists
 	let book = null;
+	let chapter : number | null = null;
 
 	// queues of values and placeholders being processed
 	// we only emit values to results once the sequence of
@@ -31,7 +32,7 @@ export function parseUrlEncoded(this: BibleRefLibData, raw: string): BibleRef[] 
 
 	let idx = 0;
 
-	function consumeInt(error: string) {
+	function consumeInt(error?: string) {
 		let out : any = null;
 		while(true) {
 			let code = raw.charCodeAt(idx);
@@ -50,7 +51,7 @@ export function parseUrlEncoded(this: BibleRefLibData, raw: string): BibleRef[] 
 
 	//console.log('parsing: ' + raw);
 	top: while(idx < raw.length) {
-		//	console.log('--------------');
+		//console.log('--------------');
 		//console.log(raw.substring(idx));
 		let book = raw.substring(idx, idx+3).toUpperCase();
 		idx += 3;
@@ -58,17 +59,18 @@ export function parseUrlEncoded(this: BibleRefLibData, raw: string): BibleRef[] 
 			throw new Error('Failed to parse url BibleRef, wanted book id, got: ' + book);
 		}
 
-		let bSep = raw.charAt(idx);
-		//console.log('bSep: ' + bSep + ', rest: ' + raw.substring(idx));
-		if(bSep === '_' || bSep === '' ) {
+		// clear out existing context for the new book
+		chapter = null;
+
+		// restart loop immediately if we've reached end of block
+		let bSep = raw.charAt(idx) || '_';
+		if(bSep === '_') {
 			results.push(mkR(book));
 			continue top;
 		}
 
-		// where the current chapter-verse specifier started
+		// record where the current chapter-verse specifier started to make error messages
 		let cvStartIdx = idx;
-		// current chaper context
-		let chapter : number | null = null;
 
 		parseChapterVerse: while(true) {
 			//console.log('Loop');
@@ -76,8 +78,40 @@ export function parseUrlEncoded(this: BibleRefLibData, raw: string): BibleRef[] 
 			//
 			// _ is used as seperator to reset to top:
 			// hence if idx is beyond end of raw, we use _ instead of the returned empty string
-			nums.push(consumeInt(`Expected int or _ seperator or end of string at char ${idx}, got: raw.charAt(idx)`));
+			let int = consumeInt();
 			let nextSep = (raw.charAt(idx++) || '_');
+
+			if(int){
+				nums.push(int);
+			} else {
+				if(seps === '-') {
+					// then this must be a cross-book range
+					let book2 = (nextSep + raw.substring(idx, idx+2)).toUpperCase();
+					idx+=2;
+					if(!BKS[book2]) {
+						throw new Error('Invalid book id in close of cross-book range, got: ' + book2);
+					}
+
+					// we need to parse optional chapter and verse number, but we
+					// don't allow any continuation after them, so this is simple
+					// (eg, gen1v2-exo3v4,5 is invalid)
+					let start : BibleRef = chapter ? { book, chapter: chapter, verse: nums.shift()! } : { book, chapter: nums.shift() || 1, verse: 1 };
+
+					//console.log("About to parse cross-book range closer cv spec: " + raw.substring(idx));
+					let chapter2 = consumeInt();
+
+					let end : BibleRef = chapter2 ? mkR(book2, chapter2).end : mkR(book2).end;
+					if(chapter2 && raw.charAt(idx) === 'v') {
+						++idx;
+						end.verse = consumeInt();
+						if(!end.verse) {
+							throw new Error("Expected integer after 'v' seperator in closing cross-book range");
+						}
+					}
+					results.push({ is_range: true, start, end });
+					continue top;
+				}
+			} // end of cross-book range parsing
 
 			//console.dir({ seps, nums, nextSep });
 
@@ -123,12 +157,10 @@ export function parseUrlEncoded(this: BibleRefLibData, raw: string): BibleRef[] 
 				if(nextSep === '_') { continue top; } else { continue parseChapterVerse; }
 			}
 
-			//console.log("  NO MATCH");
+			//console.log('no match');
 			seps += nextSep;
 		}
 	}
-
-	//console.dir(results);
 
 	return results;
 }
